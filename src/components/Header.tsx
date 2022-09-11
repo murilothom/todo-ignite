7e9b2606151eac93dccf2fc1a589e65188708331
Murilo Thom <murilothom.2002@hotmail.com>
2022-09-11 00:08:17 -0300

    feat: header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import styles from './Header.module.css'
+
+import Rocket from '../assets/rocket.svg'
+
 export function Header () {
   return (
-    <header>
-      <h1>todo</h1>
+    <header className={styles.header}>
+      <img src={Rocket} alt="Foguete" />
+      <h1 className={styles.title}>to<span>do</span></h1>
     </header>
   )
 }
