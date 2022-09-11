1b639facaead2034b764d7c02a9d03cb78697d50
Murilo Thom <murilothom.2002@hotmail.com>
2022-09-11 02:50:40 -0300

    feat: style list tasks

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 import styles from './Tasks.module.css'
 
 import { InputTask } from './InputTask'
 import { WithoutTasks } from './WithoutTasks'
+import { TaskList } from './TaskList'
+
+interface Task {
+  id: string;
+  title: string;
+  isComplete: boolean;
+}
+
+interface TaskProps {
+  tasks: Task[]
+}
 
 export function Tasks () {
+  const [tasks, setTasks] = useState([
+    {
+      id: uuidv4(),
+      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+      isComplete: false,
+    },
+    {
+      id: uuidv4(),
+      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+      isComplete: false,
+    },
+    {
+      id: uuidv4(),
+      title: 'Estudar JavaScript',
+      isComplete: true,
+    },
+  ])
+
   return (
     <section>
 
       <InputTask />
       
-      <section>
+      <article>
         <header className={styles.headerTasks}>
           <div className={styles.createdTasks}>
-            <strong>Taregas criadas</strong><span>0</span>
+            <strong>Tarefas criadas</strong><span>0</span>
           </div>
           <div className={styles.completedTasks}>
             <strong>Concluídas</strong><span>0</span>
@@ -22,9 +52,15 @@ export function Tasks () {
         </header>
         
         <main className={styles.tasksList}>
-          <WithoutTasks />
+          {tasks.length === 0 
+            ? <WithoutTasks />
+            : <ul>{(tasks.map(task => (
+                <TaskList key={task.id} task={task} />
+              )))}
+              </ul>
+          }
         </main>
-      </section>
+      </article>
 
     </section>
   )
