d446711260ae06978fdd163965c799d2978e5143
Murilo Thom <murilothom.2002@hotmail.com>
2022-09-10 21:10:28 -0300

    chore: initial config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './global.css'
+import styles from './App.module.css'
+
+export function App() {
+  return (
+    <div className={styles.wrapper}>ToDo Ignite</div>
+  )
+}
